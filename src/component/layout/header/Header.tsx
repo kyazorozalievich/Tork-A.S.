e1683cb45ab7b718e23d4f75066729806852d52c
karatalov <nursultankaratalov2@gmail.com>
2025-03-10 16:02:32 +0600

    header/hero

--- src/component/layout/header/Header.tsx
+++ src/component/layout/header/Header.tsx
@@ -1,14 +1,34 @@
-"use client";
-import scss from "./Header.module.scss";
+'use client'
+import Link from 'next/link'
+import scss from './Header.module.scss'
 
 const Header = () => {
-  return (
-    <section className={scss.Header}>
-      <div className="container">
-        <div className={scss.content}>Header</div>
-      </div>
-    </section>
-  );
-};
+	const links = [
+		{ title: 'Home', href: '/' },
+		{ title: 'Products', href: '/products' },
+		{ title: 'About', href: '/about' },
+		{ title: 'Contact', href: '/contact' },
+	]
 
-export default Header;
+	return (
+		<section className={scss.Header}>
+			<div className='container'>
+				<div className={scss.content}>
+					<h1>
+						TORK<span> A.Ş.</span>
+					</h1>
+					<div className={scss.nav}>
+						{links.map((el, idx) => (
+							<Link key={idx} href={el.href}>
+								{el.title}
+							</Link>
+						))}
+						<button>Click</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Header
--- src/component/pages/main/MainWelcome.tsx
+++ src/component/pages/main/MainWelcome.tsx
@@ -1,14 +1,41 @@
-"use client";
-import scss from "./MainWelcome.module.scss";
+'use client'
+import Image from 'next/image'
+import scss from './MainWelcome.module.scss'
 
 const MainWelcome = () => {
-  return (
-    <section className={scss.MainWelcome}>
-      <div className="container">
-        <div className={scss.content}>MainWelcome</div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section className={scss.MainWelcome}>
+			<div className='container'>
+				<div className={scss.content}>
+					<div className={scss.text}>
+						<h2>
+							Автоматизированные <br /> решения для заправки
+							<br /> газовых баллонов
+						</h2>
+						<p>
+							Мы разрабатываем и производим высококачественное оборудование для
+							наполнения <br /> газовых баллонов, обеспечивая безопасность,
+							эффективность и удобство использования. <br /> Наши технологии
+							соответствуют мировым стандартам и применяются в различных
+							отраслях. <br /> Мы осуществляем доставку по всему миру,
+							гарантируя надежность и долговечность нашей продукции. Современные
+							технологии. Высокая надежность. Доставка по всему миру.
+						</p>
+						<div className={scss.btns}>
+							<button>Изучить решения</button>
+							<button className={scss.btn__second}>Связаться с нами</button>
+						</div>
+					</div>
+					<Image
+						src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNKQtTbYMdi-crj_rajFWsQUFoPplOUfP5uw&s'
+						alt=''
+						width={350}
+						height={350}
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default MainWelcome;
+export default MainWelcome
