7450c04abf51c38d70b2c2a6a851729b482a5ffc
karatalov <nursultankaratalov2@gmail.com>
2025-03-12 14:07:32 +0600

    footerAllEnd

--- src/component/layout/footer/Footer.tsx
+++ src/component/layout/footer/Footer.tsx
@@ -1,14 +1,50 @@
-"use client";
-import scss from "./Footer.module.scss";
+'use client'
+import Link from 'next/link'
+import scss from './Footer.module.scss'
 
 const Footer = () => {
-  return (
-    <section className={scss.Footer}>
-      <div className="container">
-        <div className={scss.content}>Footer</div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section className={scss.Footer}>
+			<div className='container'>
+				<div className={scss.content}>
+					<div className={scss.tork}>
+						<h5>TORK A.S</h5>
+						<p>
+							Leading manufacturer of automatic <br /> machines for filling gas
+							cylinders.
+						</p>
+					</div>
+					<div className={scss.products}>
+						<h6>Products</h6>
+						<Link href='/'>Filling Stations</Link>
+						<Link href='/'>Testing Equipment</Link>
+						<Link href='/'>Transport Systems</Link>
+					</div>
+					<div className={scss.company}>
+						<h6>Company</h6>
+						<Link href='/'>About Us</Link>
+						<Link href='/'>Contact</Link>
+						<Link href='/'>Careers</Link>
+					</div>
+					<div className={scss.contact}>
+						<h6>Contact</h6>
+						<p>1234 Industry Avenue</p>
+						<p>Engineering District, TK 12345</p>
+						<p>email: info@torkash.com</p>
+						<p>phone: +1 (555) 123-1234</p>
+					</div>
+				</div>
+			</div>
+			<hr />
+			<div className={scss.footer}>
+				<h6>2025 TORKAS. All rights reserved</h6>
+				<div className={scss.footer__links}>
+					<Link href='/'>Privacy Policy</Link>
+					<Link href='/'>Terms Of Service </Link>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default Footer;
+export default Footer
--- src/component/pages/main/MainOther.tsx
+++ src/component/pages/main/MainOther.tsx
@@ -1,14 +1,32 @@
-"use client";
-import scss from "./MainOther.module.scss";
+'use client'
+import scss from './MainOther.module.scss'
 
 const MainOther = () => {
-  return (
-    <section className={scss.MainOther}>
-      <div className="container">
-        <div className={scss.content}>MainOther</div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section className={scss.MainOther}>
+			<div className='container'>
+				<div className={scss.content}>
+					<div className={scss.ready}>
+						<p>Ready to Transform Your Operations?</p>
+					</div>
+					<div className={scss.text}>
+						<h2>
+							Implement Modern Technologies for <br /> the Gas Industry
+						</h2>
+						<p>
+							We help businesses worldwide modernize their gas cylinder filling
+							processes. Contact us today to learn how our solutions can benefit
+							your operations.
+						</p>
+					</div>
+					<div className={scss.btns}>
+						<button>Request a Demo</button>
+						<button className={scss.active}>Download Brochure</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default MainOther;
+export default MainOther
