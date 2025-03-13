784a249b4cc24b9b7e12fc0c723b325c7b3244cd
karatalov <nursultankaratalov2@gmail.com>
2025-03-13 16:39:31 +0600

    adaptacion header

--- src/component/layout/header/Header.tsx
+++ src/component/layout/header/Header.tsx
@@ -1,145 +1,126 @@
-"use client";
-import Link from "next/link";
-import scss from "./Header.module.scss";
-import { FaAngleDown } from "react-icons/fa";
-import { useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
-import { TbWorld } from "react-icons/tb";
-import Image from "next/image";
-import england from "../../assets/images/header/flag-England.webp";
-import germany from "../../assets/images/header/flag-Germany.png";
-import france from "../../assets/images/header/flag-France.png";
-import italy from "../../assets/images/header/flag-Italy.webp";
-import arabic from "../../assets/images/header/flag-Arabic.png";
-import spain from "../../assets/images/header/flag-Spain.png";
-import logo from "../../assets/images/logo.png";
+'use client'
+import Link from 'next/link'
+import scss from './Header.module.scss'
+import { FaAngleDown } from 'react-icons/fa'
+import { useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { TbWorld } from 'react-icons/tb'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import Image from 'next/image'
+import england from '../../assets/images/header/flag-England.webp'
+import germany from '../../assets/images/header/flag-Germany.png'
+import france from '../../assets/images/header/flag-France.png'
+import italy from '../../assets/images/header/flag-Italy.webp'
+import arabic from '../../assets/images/header/flag-Arabic.png'
+import spain from '../../assets/images/header/flag-Spain.png'
+import logo from '../../assets/images/logo.png'
 
 const Header = () => {
-  const [modalCLick, setModalClick] = useState(false);
-  const [languageCLick, setlanguageClick] = useState(false);
-  const router = useRouter();
-  const pathname = usePathname();
+	const [modalCLick, setModalClick] = useState(false)
+	const [languageCLick, setlanguageClick] = useState(false)
+	const [menuOpen, setMenuOpen] = useState(false)
+	const router = useRouter()
+	const pathname = usePathname()
 
-  setTimeout(() => {
-    if (modalCLick) {
-      setModalClick(false);
-    }
-    if (languageCLick) {
-      setlanguageClick(false);
-    }
-  }, 1000);
+	setTimeout(() => {
+		if (modalCLick) {
+			setModalClick(false)
+		}
+		if (languageCLick) {
+			setlanguageClick(false)
+		}
+	}, 1000)
 
-  const products = [
-    {
-      title: "Gas cylinder refills",
-      href: "/products/refills",
-    },
-    {
-      title: "Gas filling station",
-      href: "/products/station",
-    },
-    {
-      title: "Marking of cylinders.",
-      href: "/products/marking",
-    },
-  ];
+	const products = [
+		{ title: 'Gas cylinder refills', href: '/products/refills' },
+		{ title: 'Gas filling station', href: '/products/station' },
+		{ title: 'Marking of cylinders.', href: '/products/marking' },
+	]
 
-  const countries = [
-    {
-      icon: england,
-      title: "English",
-    },
-    {
-      icon: germany,
-      title: "Germany",
-    },
-    {
-      icon: france,
-      title: "France",
-    },
-    {
-      icon: italy,
-      title: "Italy",
-    },
-    {
-      icon: arabic,
-      title: "Arabic",
-    },
-    {
-      icon: spain,
-      title: "Spain",
-    },
-  ];
+	const countries = [
+		{ icon: england, title: 'English' },
+		{ icon: germany, title: 'Germany' },
+		{ icon: france, title: 'France' },
+		{ icon: italy, title: 'Italy' },
+		{ icon: arabic, title: 'Arabic' },
+		{ icon: spain, title: 'Spain' },
+	]
 
-  return (
-    <header className={scss.Header}>
-      <div className="container">
-        <div className={scss.content}>
-          <span onClick={() => router.push("/")}>
-            <Image src={logo} alt="" className={scss.logo} />
-          </span>
-          <div className={scss.nav}>
-            <Link
-              href="/"
-              className={pathname === "/" ? scss.active : scss.link}
-            >
-              Home
-            </Link>
-            <div className={scss.product}>
-              <a className={scss.link}>
-                Products <FaAngleDown />
-              </a>
-              {!modalCLick && (
-                <div className={scss.modal}>
-                  {products?.map((item, index) => (
-                    <Link
-                      href={item.href}
-                      key={index}
-                      onClick={() => setModalClick(true)}
-                    >
-                      {item.title}
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </div>
-            <Link
-              href="/about"
-              className={pathname === "/about" ? scss.active : scss.link}
-            >
-              About
-            </Link>
-            <Link
-              href="/contact"
-              className={pathname === "/contact" ? scss.active : scss.link}
-            >
-              Contact
-            </Link>
-            <div className={scss.translation}>
-              <button>
-                <TbWorld />
-                Language
-              </button>
-              {!languageCLick && (
-                <div className={scss.languagesModal}>
-                  {countries.map((el, idx) => (
-                    <div
-                      className={scss.country}
-                      key={idx}
-                      onClick={() => setlanguageClick(true)}
-                    >
-                      <Image src={el.icon} alt="" width={20} height={10} />
-                      <h4>{el.title}</h4>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
+	return (
+		<header className={scss.Header}>
+			<div className='container'>
+				<div className={scss.content}>
+					<span onClick={() => router.push('/')}>
+						<Image src={logo} alt='' className={scss.logo} />
+					</span>
+					<button
+						className={scss.burger}
+						onClick={() => setMenuOpen(!menuOpen)}
+					>
+						<GiHamburgerMenu />
+					</button>
+					<div className={`${scss.nav} ${menuOpen ? scss.open : ''}`}>
+						<Link
+							href='/'
+							className={pathname === '/' ? scss.active : scss.link}
+						>
+							Home
+						</Link>
+						<div className={scss.product}>
+							<a className={scss.link}>
+								Products <FaAngleDown />
+							</a>
+							{!modalCLick && (
+								<div className={scss.modal}>
+									{products?.map((item, index) => (
+										<Link
+											href={item.href}
+											key={index}
+											onClick={() => setModalClick(true)}
+										>
+											{item.title}
+										</Link>
+									))}
+								</div>
+							)}
+						</div>
+						<Link
+							href='/about'
+							className={pathname === '/about' ? scss.active : scss.link}
+						>
+							About
+						</Link>
+						<Link
+							href='/contact'
+							className={pathname === '/contact' ? scss.active : scss.link}
+						>
+							Contact
+						</Link>
+						<div className={scss.translation}>
+							<button>
+								<TbWorld />
+								Language
+							</button>
+							{!languageCLick && (
+								<div className={scss.languagesModal}>
+									{countries.map((el, idx) => (
+										<div
+											className={scss.country}
+											key={idx}
+											onClick={() => setlanguageClick(true)}
+										>
+											<Image src={el.icon} alt='' width={20} height={10} />
+											<h4>{el.title}</h4>
+										</div>
+									))}
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
 
-export default Header;
+export default Header
